fada52a933312e87e53e3dca2b7f30444f864a46
Shingo Yamada <yamada-shingo@pal-relations.co.jp>
2020-01-16 19:40:47 +0900

    Implemented to build multiple pages

--- builder.py
+++ builder.py
@@ -1,20 +1,30 @@
 import yaml
 from jinja2 import Environment, FileSystemLoader
 
-env = Environment(loader=FileSystemLoader('templates'))
-tpl = env.get_template('index.html')
+env = Environment(
+    loader=FileSystemLoader('templates'),
+    #block_start_string='[%',
+    #block_end_string='%]',
+    #variable_start_string='[[',
+    #variable_end_string=']]',
+    #comment_start_string='[#',
+    #comment_end_string='#]'
+)
+
 
 with open('config.yml', 'r') as stream:
     confs = yaml.load(stream)
 
-#!!!!!!!!!!!!!!!!
-from pprint import pprint
-pprint(confs)
-#!!!!!!!!!!!!!!!!
-rendered = tpl.render(confs)
-
-with open('public/index.html', 'w') as fh:
-    fh.write(rendered)
+comon = confs['common']
+for page, vals in confs.items():
+    if page == 'common':
+        continue
+    file_name = page + '.html'
+    tpl = env.get_template(file_name)
+    vals['common'] = comon
+    rendered = tpl.render(vals)
+    with open('public/' + file_name, 'w') as fh:
+        fh.write(rendered)
 
 print('Genarated!')
 
